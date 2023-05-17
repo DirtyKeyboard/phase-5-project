134aa80747e993446c39ff1ef348fd190d0c2ae2
DirtyKeyboard <andrewxx000222@icloud.com>
2023-05-17 16:25:03 -0500

    navbar;

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import user from './assets/user.png'
 
 const NavBar = () => {
     return (
-        <div className="flex flex-row gap-4 bg-blue2 h-20vh">
-            <i>HI</i>
-        </div>
+        <nav className="flex items-center gap-4 bg-blue2 h-[4.4rem] p-2">
+            <NavLink to="/dashboard" className="nav-link">Dashboard </NavLink>
+            <NavLink to="/" className="nav-link">Random</NavLink>
+            <NavLink to="/" className="nav-link">Random</NavLink>
+            <NavLink to="/" className="nav-link">Random</NavLink>
+            <NavLink to='/my_account' className='ml-auto'><img src={user} className="w-14"/></NavLink>
+        </nav>
     )
 }
 
