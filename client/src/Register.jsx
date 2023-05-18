c3e425968af737d73f8dce5cf2ecb0ba25e6e2a4
DirtyKeyboard <andrewxx000222@icloud.com>
2023-05-17 20:41:36 -0500

    COLORS DONE

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -58,8 +58,8 @@ const Register = () => {
                     <label className="text-4xl">Password</label>
                     <input onChange={handleChange} value={form.password} name="password" className="bg-[#e4e4e4] w-[stretch] h-7" type='password' />
                     <div className="flex gap-4">
-                        <button type="submit" className="btn-default bg-iris text-white font-bold hover:bg-baby">Submit</button>
-                        <button onClick={() => {nav('/')}} className="btn-default bg-iris text-white font-bold hover:bg-baby">Go Back</button>
+                        <button type="submit" className="btn-default bg-iris text-smoke font-bold hover:bg-baby">Submit</button>
+                        <button onClick={() => {nav('/')}} className="btn-default bg-iris text-smoke font-bold hover:bg-baby">Go Back</button>
                     </div>
                 </form>
             </div>
